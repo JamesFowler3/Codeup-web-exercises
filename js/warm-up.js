66a27a6f4e9ac868ecc34f0187736372b52c1fcf
jamesfowler <JamesFowlerTX@yahoo.com>
2018-01-30 09:10:10 -0600

    added warm up

---
+++ js/warm-up.js
@@ -0,0 +1,23 @@
+(function() {
+
+    "use strict";
+
+    /*
+        ==================================================================================
+        Write a function that will return the second character of a passed in string.
+
+          example: returnSecondChar("123")      => returns "2"
+          example: returnSecondChar("123abc")   => returns "2"
+          example: returnSecondChar("aBc")      => returns "B"
+          example: returnSecondChar("a")        => returns ""
+          example: returnSecondChar("")         => returns ""
+
+        Remember to test your solution with the above example inputs.
+        Add and commit your solution in a branch called yourFirstNameHere-yourLastNameHere.
+        ==================================================================================
+    */
+
+
+
+
+})();
