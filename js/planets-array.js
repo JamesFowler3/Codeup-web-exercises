d94a312b8552a5659c2c7c4aa10e423ad85e4d55
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-01 10:40:57 -0600

    Planets exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -20,21 +20,32 @@
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
+    planets.unshift("Sun");
+    console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    console.log(planets);
+    planets.push("Pluto");
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
-
+    planets.shift("Sun");
+    console.log(planets);
     console.log('Removing "Pluto" from the end of the planets array.');
+    console.log(planets);
+    planets.pop("PLuto");
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
-
+    var index = planets.indexOf("Earth");
+    console.log(index);
     console.log("Reversing the order of the planets array.");
     console.log(planets);
-
+    planets.reverse();
+    console.log(planets);
     console.log("Sorting the planets array.");
+    console.log(planets);
+    planets.sort();
     console.log(planets);
 })();
