9a3f6217262a7d9c3437d9481bf302e53ef84ee3
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-12 13:47:07 -0600

    jquery pt.2

--- js/first-jquery.js
+++ js/first-jquery.js
@@ -1,4 +1,8 @@
 "use strict";
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+//   $ ("li").css("font-size" , "20px");
+//   $ ("li , p , h1").css("background-color" , "yellow");
+  var alertHeading = $ ("h1").html();
+  alert(alertHeading);
