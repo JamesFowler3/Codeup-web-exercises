8031d1d2d76966e1f3301996dff56da12867d4ea
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-22 10:24:43 -0600

    promises

---
+++ js/promises.js
@@ -0,0 +1,17 @@
+const wait = (timer) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Your promise resolved after " + timer + "miliseceonds");
+        }, timer);
+    });
+};
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+
+function getDateOfLastCommit (username) {
+    let dates = [];
+    fetch
+}
